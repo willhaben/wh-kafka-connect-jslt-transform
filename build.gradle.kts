107d3be8e3851d722cbb2fe81e4b9686ecdd6cd6
Stefan Fuchs <stefan.fuchs@willhaben.at>
2022-03-15 16:52:50 +0000

    DWH-1648 added gitlab-ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,22 @@
  * User Manual available at https://docs.gradle.org/7.4/userguide/building_java_projects.html
  */
 
-group "at.willhaben.kafka.connect.transforms.jslt"
-version "1.0.0"
+
+group = "at.willhaben.kafka.connect.transforms.jslt"
+version = System.getenv("VERSION") ?: "1.0.0"
 
 val javaVersion = 11
+
 
 plugins {
     kotlin("jvm") version "1.6.10"
     idea // Generates files that are used by IntelliJ IDEA, thus making it possible to open the project from IDEA
     `java-library` // Apply the java-library plugin for API and implementation separation.
+    `maven-publish`
 }
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
@@ -41,3 +45,39 @@ kotlin {
         (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(javaVersion))
     }
 }
+
+
+tasks.jar {
+    manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version
+            )
+        )
+    }
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "ArtifactoryLocal"
+            url = uri(project.properties.getOrDefault("artifactory_context", System.getenv("ARTIFACTORY_CONTEXT")).toString() + "/libs-release-local")
+            credentials {
+                username = project.properties.getOrDefault("artifactory_user", System.getenv("ARTIFACTORY_USER")).toString()
+                password = project.properties.getOrDefault("artifactory_password", System.getenv("ARTIFACTORY_PWD")).toString()
+            }
+        }
+    }
+}
+
